73f2e9cd226b60312c1ae8dccd615ad49f9d7dd5
Mikko <miguelangelferral@prometheus-4.local>
2014-10-03 13:40:31 -0500

    middle

--- app/middleware.py
+++ app/middleware.py
@@ -9,8 +9,9 @@ class Evalua:
             
     def process_view(self, request, view_func, view_args, view_kwargs):           
         if 'nombre' not in request.session and '/login/' not in request.path and '/validar/' not in request.path and '/admin/' not in request.path:
-            print 'no hay seccion'
-            return redirect('/login')
+            #print 'no hay seccion middleware'
+            #return redirect('/login')
+            return
         #if 'adm' not in request.session['tipousuario'] and '/compras/' in request.path:
         #    return redirect('/')
         #messages.error(request, 'La session a caducado.')
