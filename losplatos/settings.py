c017167f1c373377a0cb3df96746f732ecbb556f
Mikko <miguelangelferral@prometheus-4.local>
2014-10-03 15:03:53 -0500

    static

--- losplatos/settings.py
+++ losplatos/settings.py
@@ -101,7 +101,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_ROOT = '/home/static/'
+STATIC_ROOT = '/home/losplatos/webapps/static/'
 
 STATIC_URL = 'http://losplatos.webfactional.com/static/'
 
