821c2d2d50d73d712344cfcd8da22978cbd23e5c
Mikko <mferral@gmail.com>
2014-10-13 23:26:35 -0500

    insumos

--- app/templatetags/mitemplate.py
+++ app/templatetags/mitemplate.py
@@ -6,4 +6,10 @@ register = template.Library()
 def precio_impuesto(preciocosto, impuesto):
     # you would need to do any localization of the result here
     precio=float(preciocosto)+(float(preciocosto)*(float(impuesto)*0.01))
-    return precio 
+    return precio
+
+@register.simple_tag
+def multiplica(cantidad, precio):
+    # you would need to do any localization of the result here
+    preciototal=float(cantidad)*float(precio)
+    return preciototal  
